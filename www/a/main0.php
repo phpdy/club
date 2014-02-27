a9eb016f7d2d73eaa08dd9f5bc68acf9f05d13e3
dyong <dyong525@163.com>
2014-02-27 23:02:27 +0800

    update index

--- www/a/main0.php
+++ www/a/main0.php
@@ -1,6 +1,9 @@
 <div id="mainbody">
 	<div class="topimg"><img src="./images/001.jpg" width="960" height="600" alt=""></div>
 	<div id="mainc">
+		<?php 
+		for($i=0;$i<3;$i++){
+		?>
 		<div class="fenlei">
 			<ul>
 				<li style="background:#000;color:#fff">
@@ -8,84 +11,47 @@
 					2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）
 				</li>
 				
+				<?php for($h=0;$h<3;$h++){
+				?>
 				<li>
 					<div class="list">
-						<div class="list_l"><img src="./images/020.jpg" /></div>
-						<div class="list_l">2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</div>
+						<span class="list_s"><img src="./images/020.jpg" /></span>
+						<span class="list_s">2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</span>
 					</div>
 				</li>
-				<li>
-					<div class="list">
-						<div class="list_l"><img src="./images/020.jpg" /></div>
-						<div class="list_l">2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</div>
-					</div>
-				</li>
+				<?php }?>
 			</ul>
 		</div>
-		<div class="fenlei">
-			<ul>
-				<li><img src="./images/010.jpg" width="280" /></li>
-				<li>2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</li>
-				
-				<li>2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</li>
-				<li>2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</li>
-			</ul>
-		</div>
-		<div class="fenlei">
-			<ul>
-				<li><img src="./images/010.jpg" width="280" /></li>
-				<li>2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</li>
-				
-				<li>2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</li>
-				<li>2014年4月23日-5月4日 “沿着亚当斯的足迹”美西摄影集训（满员，可候补）</li>
-			</ul>
-		</div>
+		<?php } ?>
 	</div>
 </div>
 
 
 <div id="mainbody2">
 <div id="mainc2">
-	<div class="mainc_title">活动预告</div>
+	<div class="mainc_title">和我们一起探索美妙的摄影世界</div>
 	<hr>
 	<div class="mainc_img">
-	<?php for($i=0;$i<3;$i++){?>
+	<?php for($i=0;$i<=3;$i++){?>
 		<ul>
-			<li><img src="./images/img002.gif"/></li>
-			<li>&nbsp; photo workshop:london >></li>
+			<li><img src="./images/03<?php echo $i;?>.jpg"/></li>
+			<li>&nbsp; 纽摄中国学员优秀作品视频华丽上映！</li>
 			<li>&nbsp;</li>
-			<li>&nbsp; 7 Days</li>
-		</ul>
-		<ul>
-			<li><img src="./images/img003.gif"/></li>
-			<li>&nbsp; photo workshop:london >></li>
-			<li>&nbsp;</li>
-			<li>&nbsp; 7 Days</li>
 		</ul>
 	<?php }?>
 	</div>
-</div>
-</div>
-
-<div id="mainbody2">
-<div id="mainc2">
-	<div class="mainc_title">活动回顾</div>
-	<hr>
+	<div class="more"><b>更多摄影作品展示</b>></div>
+	
 	<div class="mainc_img">
-	<?php for($i=0;$i<3;$i++){?>
+	<?php for($i=0;$i<=3;$i++){?>
 		<ul>
-			<li><img src="./images/img002.gif"/></li>
-			<li>&nbsp;photo workshop:london >></li>
+			<li><img src="./images/03<?php echo 3-$i;?>.jpg"/></li>
+			<li>&nbsp; 2014年1月14日-19日吉林雾凇冰雪高端摄影实践花絮</li>
 			<li>&nbsp;</li>
-			<li>&nbsp;7 Days</li>
-		</ul>
-		<ul>
-			<li><img src="./images/img003.gif"/></li>
-			<li>&nbsp; photo workshop:london >></li>
-			<li>&nbsp;</li>
-			<li>&nbsp; 7 Days</li>
 		</ul>
 	<?php }?>
 	</div>
+	<div class="more"><b>更多过往活动花絮</b>></div>
+	
 </div>
 </div>
